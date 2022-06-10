4d1a1b5f25ddc10e593860a1c198a3964989c85b
LaraGenovese <93852155+LaraGenovese@users.noreply.github.com>
2022-06-10 12:03:42 -0300

    .

---
+++ Time/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public Text txt_time;
+    public Text txt_timeFloored;
+
+    float elapsedTime;
+
+    bool isCounting;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0;
+        isCounting = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentTime = Time.time;
+
+        txt_time.text = currentTime.ToString();
+
+        txt_timeFloored.text = Mathf.FloorToInt(currentTime).ToString();
+
+        if (isCounting)
+        {
+            elapsedTime += Time.deltaTime;
+        }
+    }
+}
